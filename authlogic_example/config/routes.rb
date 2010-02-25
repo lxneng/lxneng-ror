9dc5ca14a8f2267779278525c7b9f891b89a0b96
Eric Lo <lxneng@gmail.com>
2010-02-25 23:21:40 +0800

    authlogic_example

---
+++ authlogic_example/config/routes.rb
@@ -0,0 +1,8 @@
+ActionController::Routing::Routes.draw do |map|
+  map.login "login", :controller => "user_sessions", :action => "new"
+  map.logout "logout", :controller => "user_sessions", :action => "destroy"
+  map.resource :account, :controller => "users"
+  map.resources :users
+  map.resource :user_session
+  map.root :controller => "user_sessions", :action => "new"
+end
