564684b398af0e75de1e799b85f4703cf5a9a2c4
Eric Lo <lxneng@gmail.com>
2010-12-12 18:12:37 +0800

    devise demo

---
+++ devise-demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseDemo::Application.config.secret_token = 'c6498801c3c6ccc6fcaffe3acfb1b72f8a10020f25e37f0ec54b1d1e0fe7247d99e212d96664c11e81cdf57d1752d04594175912df9d3f043ceb1a962cd34428'
---
+++ devise-demo/config/initializers/setup_email.rb
@@ -0,0 +1,9 @@
+ ActionMailer::Base.smtp_settings = {  
+   :address              => "smtp.gmail.com",  
+   :port                 => 587,  
+   :domain               => "localhost.com",  
+   :user_name            => "lxfsddneng",  
+   :password             => "paaaasdfdsfds",  
+   :authentication       => "plain",  
+   :enable_starttls_auto => true  
+ }
