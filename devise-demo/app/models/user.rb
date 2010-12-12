79d343506b326ceedfc36678857846155cfb7a01
Eric Lo <lxneng@gmail.com>
2010-12-12 18:46:50 +0800

    login with username or email

--- devise-demo/app/models/user.rb
+++ devise-demo/app/models/user.rb
@@ -5,5 +5,16 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :login
+  
+  attr_accessor :login
+  
+  protected
+
+  def self.find_for_database_authentication(conditions)
+    value = conditions[authentication_keys.first]
+    where(["username = :value OR email = :value", { :value => value }]).first
+  end
+  
+  
 end
--- devise-demo/config/routes.rb
+++ devise-demo/config/routes.rb
@@ -1,5 +1,5 @@
 DeviseDemo::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :path_names => { :sign_up => "register" }
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
